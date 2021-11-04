1351f706dfc942c2c6ef12866933c45ba5027694
Nicholas Fagner <nicholasfagner@gmail.com>
2021-11-04 15:11:29 -0600

    commit tree 7ad7121ce8461db38276f01fa96dac8c013ac07a

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+      <h2>About Me</h2>
+      <p>Oh wow, I got to choose the content of this paragraph!  Look at me go just typing away.
+        Wow, great job me, this content is top tier!
+      </p>
+      <img src={image} alt="I made this"></img>
+  </div>;
 }
 
 export default About;
